57de36c925631432ac5bda160981856a43ca875b
victorsandru <vsandru4@gmail.com>
2023-10-06 11:21:39 -0400

    finsihed delete

--- A3/A3.hs
+++ A3/A3.hs
@@ -167,19 +167,20 @@ update key nValue (Add k v rest) =
 deleteGoody :: [Maybe Goody] -> Int -> [Maybe Goody]
 deleteGoody [] _ = [Nothing]
 deleteGoody (g:gs) n = 
-    if n == 0 then gs
+    if n == 0 then Nothing : gs
     else g : deleteGoody gs (n - 1)
 
 -- (stowGoody gs n bp) is bp if the goody at position n in gs is Nothing, else it
 -- is bp with the value of the corresponding key in bp incremented by 1.
 stowGoody :: [Maybe Goody] -> Int -> Backpack -> Backpack
 -- stowGoody [] _ _ = [Nothing]
-stowGoody (g:gs) n bp =
-    undefined
+-- stowGoody (g:gs) n bp =
+--     undefined
     -- if n == 0 then
     --     if g == Nothing then Nothing
     --     else bp
     -- else bp
+    
 -- (transition poke state) is the state resulting from moving the wombat to a new
 -- position and collecting the goody there, if any. "Collecting the goody"
 -- involves updating the path to show the goody is gone, and updating the
